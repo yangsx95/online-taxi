3ff268dd71169413a633169d74bca5293060affd
杨顺翔 <yangsx95@qq.com>
2022-07-23 19:48:31 +0800

    feat: 验证码服务

---
+++ common-server/verification-code/verification-code-api/src/main/java/com/yangsx95/onlinetaxi/verificationcode/api/VerificationCodeApi.java
@@ -0,0 +1,12 @@
+package com.yangsx95.onlinetaxi.verificationcode.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * 验证码获取API
+ *
+ * @author 杨顺翔
+ * @since 2022/07/23
+ */
+public interface VerificationCodeApi {
+}
---
+++ common-server/verification-code/verification-code-starter/src/main/java/com/yangsx95/onlinetaxi/verificationcode/VerificationCodeApplication.java
@@ -0,0 +1,17 @@
+package com.yangsx95.onlinetaxi.verificationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 杨顺翔
+ * @since 2022/07/23
+ */
+@SpringBootApplication
+public class VerificationCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationCodeApplication.class, args);
+    }
+    
+}
