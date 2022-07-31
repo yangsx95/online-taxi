bd1dd089a1075a22f0877b0f3c861dec6b8c9986
杨顺翔 <yangsx95@qq.com>
2022-08-01 07:39:23 +0800

    online taxi修改基础包

--- infrastructure/online-taxi-spring-boot-autoconfigure/src/main/java/com/yangsx95/onlinetaxi/autoconfigure/feign/FeignAutoConfiguration.java
+++ infrastructure/online-taxi-spring-boot-autoconfigure/src/main/java/com/yangsx95/onlinetaxi/autoconfigure/feign/FeignAutoConfiguration.java
@@ -1,7 +1,10 @@
 package com.yangsx95.onlinetaxi.autoconfigure.feign;
 
 import feign.Feign;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 2022/07/31
  */
 @Configuration
-@ConditionalOnClass(Feign.class)
+@ConditionalOnBean(EnableFeignClients.class)
 public class FeignAutoConfiguration {
 
     @Bean
--- infrastructure/online-taxi-spring-boot-autoconfigure/src/main/java/com/yangsx95/onlinetaxi/autoconfigure/feign/FeignRequestInterceptor.java
+++ infrastructure/online-taxi-spring-boot-autoconfigure/src/main/java/com/yangsx95/onlinetaxi/autoconfigure/feign/FeignRequestInterceptor.java
@@ -1,7 +1,11 @@
 package com.yangsx95.onlinetaxi.autoconfigure.feign;
 
+import com.yangsx95.onlinetaxi.autoconfigure.contstants.HttpHeader;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+
+import static com.yangsx95.onlinetaxi.autoconfigure.contstants.HttpHeader.SERVICE_TYPE;
+import static com.yangsx95.onlinetaxi.autoconfigure.contstants.HttpHeader.SERVICE_TYPE_INNER_SERVICE;
 
 /**
  * feign的请求拦截器拦截器
@@ -13,6 +17,6 @@ public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
         // 服务类型为内部服务
-        requestTemplate.header("S-X-Service-Type", "service");
+        requestTemplate.header(SERVICE_TYPE, SERVICE_TYPE_INNER_SERVICE);
     }
 }
